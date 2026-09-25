40e63aa814c877d15363ceec3f8ef029859ee7df
Javier Ayarza <jab0024@alu.medac.es>
2026-09-25 11:01:07 +0200

    25/09

---
+++ Bloque1/Ejercicio4.java
@@ -0,0 +1,42 @@
+
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        
+        try {
+            
+            BufferedInputStream entrada = new BufferedInputStream(new FileInputStream("foto.jpg"));
+
+            BufferedOutputStream salida = new BufferedOutputStream(new FileOutputStream("foto_copia_buffer.jpg"));
+
+            byte[] buffer = new byte[1024];
+
+            int bytesLeidos;
+            int bloque = 1;
+
+            while ((bytesLeidos = entrada.read(buffer)) != -1) {
+                
+                salida.write(buffer, 0, bytesLeidos);
+                System.out.println("Fin copia bloque " + bloque);
+                bloque++;
+            }
+
+            salida.flush();
+
+            entrada.close();
+            salida.close();
+
+            System.out.println("Copia finalizada correctamente");
+
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+}
---
+++ Bloque1/Ejercicio5.java
@@ -0,0 +1,5 @@
+package Bloque1.TEMA 1;
+
+public class Ejercicio5 {
+    
+}
